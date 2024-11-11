d7217fc4746a5ebdcab1d53603780f497ee18f17
Chara-X <1719552189@qq.com>
2024-11-12 04:34:00 +0800

    Up

--- Buffer.go
+++ Buffer.go
@@ -2,6 +2,7 @@ package bytes
 
 import (
 	"bytes"
+	"io"
 )
 
 type Buffer struct {
@@ -14,19 +15,30 @@ func NewBuffer(buf []byte) *Buffer {
 	if Reference {
 		return &Buffer{b: bytes.NewBuffer(buf)}
 	}
-	panic("not implemented")
+	return &Buffer{buf: buf}
 }
 func (b *Buffer) Read(p []byte) (n int, err error) {
 	if Reference {
 		return b.b.Read(p)
 	}
-	panic("not implemented")
+	if b.off >= len(b.buf) {
+		b.buf, b.off = b.buf[:0], 0
+		return 0, io.EOF
+	}
+	n = copy(p, b.buf[b.off:])
+	b.off += n
+	return n, nil
 }
 func (b *Buffer) Write(p []byte) (n int, err error) {
 	if Reference {
 		return b.b.Write(p)
 	}
-	panic("not implemented")
+	if len(b.buf)+len(p) > cap(b.buf) {
+		copy(b.buf, b.buf[b.off:])
+		b.buf = b.buf[:len(b.buf)-b.off]
+	}
+	b.buf = append(b.buf, p...)
+	return len(p), nil
 }
 func (b *Buffer) Bytes() []byte {
 	if Reference {
